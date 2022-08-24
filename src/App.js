c7b57b5eccae7b1f8f508423421351a63a3a879f
sandeep pangeni <pangenisandeep2133@gmail.com>
2022-08-24 12:09:47 +0545

    first client project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+ const App = () => {
 
-function App() {
+  const url = "https://jsonplaceholder.typicode.com/posts"
+  
+  const [showData, setShowData] = useState([]);
+
+  const loadData = () =>{
+    fetch(url, {
+      method: "GET",
+    })
+    .then((res) => res.json())
+    .then((result) => {
+      if (result) {
+        setShowData((result));
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  };
+
+  useEffect(() =>{
+    loadData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+     <h5>React Accordion</h5> 
+    
+    <div className='row'>
+
+        {showData.map((item,index))}
+
     </div>
-  );
+
+    </div>
+  )
 }
 
-export default App;
+export default App
